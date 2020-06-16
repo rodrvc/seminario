a878bdb4beb8b6ea782437a8c36a8da4b1d10361
rodrvc <silver.rvc@gmail.com>
2020-06-16 02:17:18 -0400

    add css

--- src/pages/landing/Login.js
+++ src/pages/landing/Login.js
@@ -1,12 +1,28 @@
 import React from "react";
 import SingUp from "../../components/SingUp";
+import { Button, Radio } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 export function Login() {
   return (
-    <div>
-      <h1>This is a login</h1>
-      <div className="container">
-        <h1>Registrate</h1>
+    <div className="mainfront">
+      <div className="front">
+        <div>
+          <h1 className="langing-title">¿Buscando un Servicio?</h1>
+        </div>
+        <div className="landing-msj-container">
+          <p className="langing-msj">Todos tenemos algo que ofrecer...</p>
+        </div>
+        <Button
+          className="landing-btn"
+          type="primary"
+          icon={<DownloadOutlined />}
+          size={"large"}
+        >
+          EXPLORAR
+        </Button>
+      </div>
+      <div className="front landing-form">
         <SingUp />
       </div>
     </div>
