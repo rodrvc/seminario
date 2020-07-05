283735fdfae3f0bce73bda4fd24cc9f038a60f95
rodrigo <silver.rvc@gmail.com>
2020-07-05 02:50:47 -0400

    mejora valicacion login

--- src/api/user.js
+++ src/api/user.js
@@ -1,26 +1,30 @@
 import { BASE_PATH } from "./config";
 
 export function signUpApi(data) {
-  const url = `${BASE_PATH}/users`;
-  const params = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+	const url = `${BASE_PATH}/users`;
+	const params = {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
 
-  return fetch(url, params)
-    .then((response) => {
-      return response; // not convert to json!
-    })
-    .then((result) => {
-      if (result) {
-        return { ok: true, message: "Usuario creado correctamente" };
-      }
-      return { ok: false, message: result.message + "erro 1 " };
-    })
-    .catch((err) => {
-      return { ok: false, err };
-    });
+	return fetch(url, params)
+		.then((response) => {
+			return response; // not convert to json!
+		})
+		.then((result) => {
+			if (result) {
+				return { ok: true, message: "Usuario creado correctamente" };
+			}
+			return { ok: false, message: result.message + "erro 1 " };
+		})
+		.catch((err) => {
+			return { ok: false, err };
+		});
+}
+
+export function signin(data) {
+	console.log(data);
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,13 +1,26 @@
 import { Form, Input, Button, Checkbox, Card } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import React, { useState } from "react";
+import React, { useState, isValidElement } from "react";
+import { signin } from "../../api/user";
 import "./SignIn.css";
+import { getFieldId } from "antd/lib/form/util";
+import SingUp from "../SingUp";
 
 const SignIn = () => {
-	const onFinish = (values) => {
-		console.log("Received values of form: ", values);
+	const [form] = Form.useForm();
+
+	const onFinish = async (values) => {
+		//console.log("Received values of form: ", values);
+		const result = await signin(values);
 	};
 
+	const [inputs, setInput] = useState({
+		//  borrar despues
+		email: "",
+		password: "",
+	});
+
+	const onchangeInput = (input) => {};
 	return (
 		<Card
 			title="INGRESA!"
@@ -19,29 +32,41 @@ const SignIn = () => {
 		>
 			<Form
 				name="normal_login"
+				layout="vertical"
 				className="login-form"
 				initialValues={{
 					remember: true,
 				}}
+				form={form}
 				onFinish={onFinish}
 				size={"large"}
 			>
 				<Form.Item
-					name="username"
+					name="email"
+					label="Correo :"
 					rules={[
 						{
 							required: true,
-							message: "Please input your Username!",
+							message: "Ingresa tu email !",
+							validateTrigger: "onBlur",
+							initialValues: "",
+						},
+						{
+							type: "email",
+							message: "Debes ingresar un email valido!",
 						},
 					]}
+					//onBlur={() => form.validateTrigger({ onBlur: "Hi, man!" })}
+					validateTrigger="onBlur"
 				>
 					<Input
 						prefix={<UserOutlined className="site-form-item-icon" />}
-						placeholder="Username"
+						placeholder="Correo electronico"
 					/>
 				</Form.Item>
 				<Form.Item
 					name="password"
+					label="Contraseña"
 					rules={[
 						{
 							required: true,
@@ -57,7 +82,7 @@ const SignIn = () => {
 				</Form.Item>
 				<Form.Item>
 					<Form.Item name="remember" valuePropName="checked" noStyle>
-						<Checkbox>Remember me</Checkbox>
+						<Checkbox>Recuerdame</Checkbox>
 					</Form.Item>
 					<a className="login-form-forgot block" href="">
 						Olvide mi contraseña
@@ -71,7 +96,7 @@ const SignIn = () => {
 						className="login-form-button"
 						block
 					>
-						Log in
+						Ingresar!
 					</Button>
 					O <a href="">Registrate Ahora!</a>
 				</Form.Item>
