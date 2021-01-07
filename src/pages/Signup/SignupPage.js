d80c4479599ad0600a5d2b2778d8a0d62086bf13
rodrigo <silver.rvc@gmail.com>
2021-01-07 01:53:20 -0300

    return to proyect fix

--- src/pages/Signup/SignupPage.js
+++ src/pages/Signup/SignupPage.js
@@ -1,33 +1,34 @@
 import React from "react";
 import SignUp from "../../components/SingUp";
-import { Button, Radio } from "antd";
-import { DownloadOutlined, SearchOutlined } from "@ant-design/icons";
+import {Button, Radio} from "antd";
+import {DownloadOutlined, SearchOutlined} from "@ant-design/icons";
 import "./singuppage.css";
 import logo from "../../assets/cooking.svg";
 import iconDoggi from "../../assets/aseo-de-mascotas.svg";
 
 function SignupPage() {
-	return (
-		<div className="register">
-			<div className="front">
-				<div>
-					<h1 className="langing-title">¿Buscando un Servicio?</h1>
-				</div>
-				<div className="landing-msj-container">
-					<p className="langing-msj">&#x21A7; Registrate Aqui &#x21A7; </p>
-				</div>
-			</div>
-			<div className="register-form">
-				<SignUp />
-			</div>
-			<div className="btn-explore">
+    return (
+        <div className="register">
+            <div className="front">
+                <div>
+                    <h1 className="langing-title">¿Buscando un Servicio?</h1>
+                </div>
+                <div className="landing-msj-container">
+                    <p className="langing-msj">&#x21A7; Registrate Aqui &#x21A7;
+                    </p>
+                </div>
+            </div>
+            <div className="register-form">
+                <SignUp/>
+            </div>
+            <div className="btn-explore">
 				<Button
 					className="landing-btn"
 					type="primary"
 					icon={<SearchOutlined spin={false} style={{ fontSize: "24px" }} />}
 					size={"large"}
 				>
-					EXPLORAR
+					Explorar
 				</Button>
 			</div>
 			<div className="icon">
@@ -35,7 +36,7 @@ function SignupPage() {
 				<img className="icon-item doggi" src={iconDoggi} alt="logo" />
 			</div>
 		</div>
-	);
+    );
 }
 
 export default SignupPage;
